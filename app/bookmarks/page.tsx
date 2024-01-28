ac040df1e00d1e4040820937230806a53c1374b5
Hugo Tiburcio <hugotiburcio@outlook.com>
2024-01-28 10:54:49 -0300

    feat: add bookmarks page

---
+++ app/bookmarks/page.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+function BookmarkedPage() {
+  return (
+    <>
+      <div>Search for bookmarked shows</div>
+
+      <div>Bookmarked Movies</div>
+
+      {/* Display bookmarked movies */}
+
+      <div>Bookmarked TV Series</div>
+
+      {/* Display bookmarked TV series */}
+    </>
+  );
+}
+
+export default BookmarkedPage;
