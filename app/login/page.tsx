3eb3f81fa2bca908f8ad5b9ac6ef20faf2a15a5e
Hugo Tiburcio <hugotiburcio@outlook.com>
2024-01-28 10:55:04 -0300

    feat: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,32 @@
+
+export default function Login () {
+  return (
+    <>
+      <html lang="en">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+          <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon-32x32.png" />
+
+          <title>Frontend Mentor | Entertainment web app</title>
+        </head>
+        <body>
+          <div>
+            Login
+          </div>
+          <div>
+            Email address
+            Password
+          </div>
+          <div>
+            Login to your account
+          </div>
+          <div>
+            Don&apos;t have an account? Sign Up
+          </div>
+        </body>
+      </html>
+    </>
+  );
+};
