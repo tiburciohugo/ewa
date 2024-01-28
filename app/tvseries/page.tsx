0a29d109f18662b3254109a80785a05b76d68f0e
Hugo Tiburcio <hugotiburcio@outlook.com>
2024-01-28 10:55:42 -0300

    feat: add tvseries page

---
+++ app/tvseries/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TVSeriesPage() {
+  return (
+    <div>
+      Search for TV series TV Series
+      {/* Display all TV series */}
+    </div>
+  );
+}
