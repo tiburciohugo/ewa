a1a7bee273ac7210c8fbf7393ab1448957bc2759
Hugo Tiburcio <hugotiburcio@outlook.com>
2024-01-30 20:15:29 -0300

    feat: add searchbar and sidebar to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Searchbar from "@/components/searchbar";
+import Sidebar from "@/components/sidebar";
 import Trending from "@/components/trending";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <section className="flex h-screen w-screen flex-col">
+      <Sidebar />
+      <Searchbar />
       <Trending />
       <div className="flex h-full w-full flex-col items-center justify-center space-y-4">
         <Image
