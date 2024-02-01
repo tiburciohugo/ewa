60064735a1783768c86b326270fa7a3f784e1b3c
Hugo Tiburcio <hugotiburcio@outlook.com>
2024-02-01 12:42:13 -0300

    chore: add new animation to icons

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -8,8 +8,11 @@ import BookmarkIcon from "../public/icon-nav-bookmark.svg";
 
 export default function Sidebar() {
   return (
-    <nav className="absolute top-0 flex w-full items-center justify-between bg-navy-blue p-4 md:max-w-[719px] md:rounded-xl lg:left-0 lg:my-4 lg:ml-4 lg:h-full lg:max-h-[960px] lg:w-24 lg:flex-col md:m-[3%]">
-      <Link href="/" className="inline-block cursor-pointer hover:opacity-80">
+    <nav className="absolute top-0 flex w-full items-center justify-between bg-navy-blue p-4 md:m-[3%] md:max-w-[719px] md:rounded-xl lg:left-0 lg:my-4 lg:ml-4 lg:h-full lg:max-h-[960px] lg:w-24 lg:flex-col">
+      <Link
+        href="/"
+        className="inline-block cursor-pointer transition delay-200 ease-in-out hover:opacity-80"
+      >
         <Image
           src={"/logo.svg"}
           alt="logo"
@@ -23,22 +26,22 @@ export default function Sidebar() {
         <ul className="flex space-x-4 lg:flex-col lg:justify-center lg:justify-items-center lg:space-x-0 lg:space-y-6">
           <li>
             <Link href="/">
-              <HomeIcon className="text-steel-blue hover:text-white active:text-white" />
+              <HomeIcon className="text-steel-blue transition delay-200 ease-in-out hover:text-white active:text-white" />
             </Link>
           </li>
           <li>
             <Link href={"/movies"}>
-              <MoviesIcon className="text-steel-blue hover:text-white active:text-white" />
+              <MoviesIcon className="text-steel-blue transition delay-200 ease-in-out hover:text-white active:text-white" />
             </Link>
           </li>
           <li>
             <Link href={"/tvseries"}>
-              <TvIcon className="text-steel-blue hover:text-white active:text-white" />
+              <TvIcon className="text-steel-blue transition delay-200 ease-in-out hover:text-white active:text-white" />
             </Link>
           </li>
           <li>
             <Link href={"/bookmarks"}>
-              <BookmarkIcon className="text-steel-blue hover:text-white active:text-white" />
+              <BookmarkIcon className="text-steel-blue transition delay-200 ease-in-out hover:text-white active:text-white" />
             </Link>
           </li>
         </ul>
