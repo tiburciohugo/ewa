298ce6ca463ecbccb2895b3e52e271a7143304f1
Hugo Tiburcio <hugotiburcio@outlook.com>
2024-02-01 19:18:09 -0300

    feat: update bookmarks page + add layout

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,19 +1,53 @@
-import Head from "next/head";
+import BookmarkButton from "@/components/ui/bookmarkbutton";
+import { Movie } from "../types/types";
+import { promises as fs } from "fs";
+import Image from "next/image";
+import MoviesIcon from "../../public/icon-nav-movies.svg";
+import TvIcon from "../../public/icon-nav-tv-series.svg";
 
-function BookmarkedPage() {
+export default async function BookmarkedPage() {
+  const file = await fs.readFile(
+    process.cwd() + "/../ewa/data/data.json",
+    "utf8",
+  );
+  const data: Movie[] = JSON.parse(file);
+
+  const bookmarked = data.filter((movie) => movie.isBookmarked === true);
   return (
-    <>
-      <div>Search for bookmarked shows</div>
+    <div className="lg:max-w-xxl w-full px-4 py-6 text-white md:max-w-screen-md md:px-6 lg:pl-36">
+      <h1 className="text-xl tracking-wider">Favorites</h1>
 
-      <div>Bookmarked Movies</div>
+      <div className="grid w-full max-w-[1440px] grid-cols-2 gap-1 md:grid-cols-3 md:gap-4 lg:w-screen lg:grid-cols-4">
+        {bookmarked?.map((movie: Movie) => (
+          <div key={movie.title} className="relative mt-4 w-full max-w-[280px]">
+            <BookmarkButton className="left-[75%] md:left-[80%]" />
+            <Image
+              className="min-h-[110px] w-full min-w-[140px] max-w-[280px] cursor-pointer rounded-xl object-cover transition duration-300 ease-in-out hover:scale-105"
+              src={`/${movie.thumbnail.regular.large}`}
+              alt={movie.title}
+              width={160}
+              height={110}
+            ></Image>
 
-      {/* Display bookmarked movies */}
+            <div className="mt-2 flex items-center text-gray-400">
+              <p>{movie.year}</p>
+              <p className="2sm:mx-2 mx-1">•</p>
+              {movie.category === "Movie" ? (
+                <MoviesIcon className="2sm:mr-2 mr-1" />
+              ) : (
+                <TvIcon className="2sm:mr-2 mr-1" />
+              )}
+              <p>{movie.category}</p>
+              <p className="2sm:mx-2 mx-1">•</p>
+              <p>{movie.rating}</p>
+            </div>
 
-      <div>Bookmarked TV Series</div>
-
-      {/* Display bookmarked TV series */}
-    </>
+            <h2 className="text-sm font-bold md:text-2xl md:font-light">
+              {movie.title}
+            </h2>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
-
-export default BookmarkedPage;
