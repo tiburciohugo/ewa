7c963dbf9b018766c184c30f2fd180fce4cb013e
Hugo Tiburcio <hugotiburcio@outlook.com>
2024-02-16 18:45:36 -0300

    fix: change play button position to inline styles

--- components/ui/playButton.tsx
+++ components/ui/playButton.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import PlayIcon from "../../public/icon-play.svg";
 
 type Props = {
-    className?: string;
-    top?: string;
-    left?: string;
-    };
+  className?: string;
+  top?: string;
+  left?: string;
+};
 
-export default function PlayButton({className, top, left}: Props ) {
+export default function PlayButton({ className, top, left }: Props) {
   return (
     <button
       type="button"
-      className={`absolute z-40 hidden cursor-pointer items-center gap-4 rounded-full bg-white bg-opacity-20 px-3 py-2 lg:left-[${left}] lg:top-[${top}] lg:group-hover:flex ${className}`}
+      style={{ position: "absolute", top: top, left: left }}
+      className={`z-40 hidden cursor-pointer items-center gap-4 rounded-full bg-white bg-opacity-20 px-3 py-2 lg:group-hover:flex ${className}`}
     >
       <PlayIcon />
       <p className="font-medium text-white">Play</p>
