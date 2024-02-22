835f3f31c121a84251e35fc08a487626480a21cb
Hugo Tiburcio <hugotiburcio@outlook.com>
2024-02-22 17:57:11 -0300

    feat: add logout button

---
+++ components/logoutForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+export default function LogoutForm() {
+  return (
+      <form action="">
+          <Button>Log out</Button>
+    </form>
+  )
+}
